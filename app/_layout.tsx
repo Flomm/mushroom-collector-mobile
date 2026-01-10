07d51d1204efdbed634fd3d8a159ac792bf3ab7c
Flomm <florian.baranya@gmail.com>
2026-01-10 23:04:40 +0100

    husky check

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,21 +4,24 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useEffect } from 'react';
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: '(tabs)'
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  useEffect();
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack.Screen name='modal' options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </ThemeProvider>
   );
 }
