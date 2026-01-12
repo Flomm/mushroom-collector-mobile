68e2018ddbf8131bed55c247cbc8319da21990f0
Flomm <florian.baranya@gmail.com>
2026-01-12 17:32:23 +0100

    auth basics added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
+// import { getAuth, onAuthStateChanged } from '@react-native-firebase/auth';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
+import { SessionProvider, useSession } from '@/context/auth-context';
+import { SplashScreenController } from '@/functions/splash-controller';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+// import { useEffect } from 'react';
 
 export const unstable_settings = {
   anchor: '(tabs)'
@@ -11,14 +15,36 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  // const [user, setUser] = useState();
+  const { session } = useSession();
+
+  // function handleAuthStateChanged(user) {
+  //   console.warn(user);
+  //   setUser(user);
+  // }
+
+  // useEffect(() => {
+  //   console.warn(getAuth());
+  //   const subscriber = onAuthStateChanged(getAuth(), handleAuthStateChanged);
+  //   return subscriber; // unsubscribe on unmount
+  // }, []);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-        <Stack.Screen name='modal' options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style='auto' />
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <SplashScreenController />
+        <Stack>
+          <Stack.Protected guard={!!session}>
+            <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+            <Stack.Screen name='modal' options={{ presentation: 'modal', title: 'Modal' }} />
+          </Stack.Protected>
+
+          <Stack.Protected guard={!session}>
+            <Stack.Screen name='login' options={{ headerShown: false }} />
+          </Stack.Protected>
+        </Stack>
+        <StatusBar style='auto' />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
