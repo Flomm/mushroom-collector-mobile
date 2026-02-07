384d450b07ff2c09f50674c4c86b72282736efa6
Flomm <florian.baranya@gmail.com>
2026-02-07 16:09:51 +0100

    remove expo generated components

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,25 @@
+import { Text } from '@tamagui/core';
 import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        headerShown: false,
-        tabBarButton: HapticTab
+        headerShown: false
       }}>
       <Tabs.Screen
         name='index'
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name='house.fill' color={color} />
+          tabBarIcon: ({ color }) => <Text>Home</Text>
         }}
       />
       <Tabs.Screen
         name='explore'
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name='paperplane.fill' color={color} />
+          tabBarIcon: ({ color }) => <Text>Explore</Text>
         }}
       />
     </Tabs>
