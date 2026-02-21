ac87296b5e590e1e27a247b5f9507cd458f024bc
Flomm <florian.baranya@gmail.com>
2026-02-21 16:02:38 +0100

    orientation.context added

---
+++ context/orientation-context/orientation.context.spec.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react-native';
+import type { ReactElement } from 'react';
+import * as ReactNative from 'react-native';
+import { Text } from 'react-native';
+import { OrientationProvider, useOrientation } from './orientation.context';
+
+jest.mock('react-native/Libraries/Utilities/useWindowDimensions', () => ({
+  default: jest.fn()
+}));
+
+const renderWithContext = (children: ReactElement) => render(<OrientationProvider>{children}</OrientationProvider>);
+
+const OrientationTestComponent = (): ReactElement => {
+  const orientation = useOrientation();
+  return <Text testID='layout-test'>{orientation}</Text>;
+};
+
+describe('OrientationContext', () => {
+  let useWindowDimensionsSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    useWindowDimensionsSpy = jest.spyOn(ReactNative, 'useWindowDimensions');
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should provide landscape value to children when screen height is smaller than width', () => {
+    useWindowDimensionsSpy.mockReturnValue({ width: 20, height: 10 });
+    renderWithContext(<OrientationTestComponent />);
+    const testText = screen.getByTestId('layout-test');
+    expect(testText).toHaveTextContent('landscape');
+  });
+
+  it('should provide portrait value to children when screen height is bigger than width', () => {
+    useWindowDimensionsSpy.mockReturnValue({ width: 10, height: 20 });
+
+    renderWithContext(<OrientationTestComponent />);
+    const testText = screen.getByTestId('layout-test');
+    expect(testText).toHaveTextContent('portrait');
+  });
+
+  it('should provide correct value to children when screen height is equal to width', () => {
+    useWindowDimensionsSpy.mockReturnValue({ width: 20, height: 20 });
+
+    renderWithContext(<OrientationTestComponent />);
+    const testText = screen.getByTestId('layout-test');
+    expect(testText).toHaveTextContent('landscape');
+  });
+});
---
+++ context/orientation-context/screen-orientation.type.ts
@@ -0,0 +1,1 @@
+export type ScreenOrientationType = 'portrait' | 'landscape';
