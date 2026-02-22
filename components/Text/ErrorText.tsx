0211fdb5d015e1e87a997ba936e1922556e21b73
Flomm <florian.baranya@gmail.com>
2026-02-22 11:10:33 +0100

    header and error texts

---
+++ components/Text/ErrorText.tsx
@@ -0,0 +1,7 @@
+import { SizableText, styled } from 'tamagui';
+
+export const ErrorText = styled(SizableText, {
+  color: '$errorColor',
+  fontWeight: '700',
+  fontFamily: '$body'
+});
---
+++ components/Text/HeaderText.tsx
@@ -0,0 +1,8 @@
+import { SizableText, styled } from 'tamagui';
+
+export const HeaderText = styled(SizableText, {
+  color: '$primaryTextColor',
+  fontWeight: '700',
+  fontFamily: '$body',
+  fontSize: '$7'
+});
